b412f5ef6f54eedc47ee926fdb00b1e976dff2cb
PrasannaSubramani <prasannasubramani1311@gmail.com>
2024-09-24 11:16:02 +0530

    Changes

--- src/components/molecules/index.jsx
+++ src/components/molecules/index.jsx
@@ -3,6 +3,6 @@
 // // // Headers.tsx
 // // // GenericTabs.jsx
 import GenericTabs  from './GenericTabs';
-import Filter from './Filters';
+import Filter from './Filter';
 
 export {GenericTabs, Filter};
