53e5b15d224692fd7fcf0ffb14b29bb7af9a87f6
bhavyash02 <bhavya.shah@accionlabs.com>
2024-09-26 15:10:52 +0530

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { REACT_APP_GOOGLE_CLIENT_ID } from "./config/index.jsx";
+import {DashboardLayoutBasic} from './components/organisms/NavBar.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeContext>
       <GoogleOAuthProvider clientId={REACT_APP_GOOGLE_CLIENT_ID}>
+        <DashboardLayoutBasic />
         <App />
       </GoogleOAuthProvider>
     </ThemeContext>
