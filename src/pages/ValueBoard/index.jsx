aa1c6882f862aeb4dde0f30ac99a093cb1ae9db7
bhavyash02 <bhavya.shah@accionlabs.com>
2024-10-22 00:42:57 +0530

    tabs-added

--- src/pages/ValueBoard/index.jsx
+++ src/pages/ValueBoard/index.jsx
@@ -1,10 +1,42 @@
 import { Box, Typography } from "@mui/material";
+import { GenericTabs } from "../../components/molecules/index";
 
 const ValueBoard = () => {
+
+  const tabs = [ 
+    {
+      index: 0,
+      label: "Name",
+      Component: 1
+    },
+    {
+      index: 1,
+      label: "Name",
+      Component: "2"
+    },
+    {
+      index: 2,
+      label: "Name",
+      Component: "e"
+    },
+    {
+      index: 3,
+      label: "Name",
+      Component: "4"
+    },
+    {
+      index: 4,
+      label: "Name",
+      Component: "4"
+    },
+    {
+      index: 5,
+      label: "Name",
+      Component: "1"
+    }
+  ]
   return (
-    <Box>
-      <Typography>Value Board</Typography>
-    </Box>
+      <GenericTabs tabs={tabs} />
   );
 };
 
