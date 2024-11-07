e173d9cab8ee00a8df2fed1a9fb0bb904a18c989
anoop-tb <121123354+anoop-tb@users.noreply.github.com>
2024-11-07 12:07:51 +0530

    add new API

--- src/config/apiUrlConfig.jsx
+++ src/config/apiUrlConfig.jsx
@@ -1,5 +1,6 @@
 const apiUrlConfig = {
-    apiUrl: 'http://192.168.168.50:8012',
-  };
-  
-  export default apiUrlConfig;
+  apiUrl: "https://intranet.accionlabs.com/pmoreporting/",
+  // apiUrl: 'http://192.168.168.50:8012', //local
+};
+
+export default apiUrlConfig;
